8cfab3dbf45bf0be1a8c8257eb669c296188a2a4
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-29 22:02:09 +0900

    コンポーネントを追加する。

---
+++ client/src/Home.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+// コンポーネントを用意する。
+const Home = () => {
+    useEffect (() => {
+
+    }, []);
+
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ client/src/NewFundraiser.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+// コンポーネントを用意する。
+const NewFundraiser = () => {
+    useEffect (() => {
+
+    }, []);
+
+    return (
+        <div>
+            <h2>Create a New Fundraiser</h2>
+        </div>
+    );
+}
+
+export default NewFundraiser;
