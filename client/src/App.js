612a6bcfe0271b510037c6378f4dea5f7789f13e
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-29 21:41:55 +0900

    App.jsを更新しました。

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,50 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import FundraiserFactoryContract from "./contracts/FundraiserFactory.json";
 import getWeb3 from "./getWeb3";
-
 import "./App.css";
 
+// コンポーネントを用意する。
+const App = () => {
+  // ステート変数を用意する。　
+  const [state, setState] = useState({ web3: null, accounts: null, contract: null  });
+
+  // useeffect関数
+  useEffect (() => {
+    const init = async () => {
+      try {
+        // 変数を設定
+        const web3 = await getWeb3();
+        const accounts = await web3.eth.getAccounts();
+        const networkId = await web3.eth.net.getId();
+        // デプロイ済みネットワークを取得する。
+        const deployedNetwork = FundraiserFactoryContract.networks[networkId];
+        // コントラクトのインスタンスを生成する。
+        const instance = new web3.eth.Contract (FundraiserFactoryContract.abi, deployedNetwork && deployedNetwork.address);
+        // ステート変数を設定する。
+        setState ({ web3, accounts, contract: instance });
+      } catch (error) {
+        // アラートを出す。
+        alert (`App.js: Failed to load web3, accounts, or contract. Check console for details.`,);
+        // アラート内容を出力する。
+        console.error (error);
+      }
+    } 
+    init();
+  }, []);
+
+  // runExample関数
+  const runExample = async() => {
+    const { accounts, contract } = state;
+  };
+
+  return (
+    <div>
+      <h1>Fundraiser</h1>
+    </div>
+  );
+}
+
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -70,4 +109,5 @@ class App extends Component {
   }
 }
 
+// コンポーネントを外部に公開
 export default App;
