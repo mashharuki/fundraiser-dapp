3b8b6825b9399302ece70fa6565cf83b1c645598
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-05-17 21:08:13 +0900

    マルチシグコントラクトの追加と署名用のページを追加しました。

---
+++ client/src/Sign.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import detectEthereumProvider from '@metamask/detect-provider';
+import SimpleMultiSigContract from './contracts/SimpleMultiSig.json';
+import Web3 from "web3";
+
+// コンポーネントを用意する。
+const Sign = () => {
+
+    // 戻り値
+    return (
+        <div className="main-container">
+            
+        </div>
+    );
+}
+
+// Signコンポーネントを外部に公開しました。
+export default Sign;
