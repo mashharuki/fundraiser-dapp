beb39ad440a191f6433222452c35d4485f388d3e
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-05-18 23:01:41 +0900

    デプロイできるように修正

--- migrations/3_deploy_simplemultisig.js
+++ migrations/3_deploy_simplemultisig.js
@@ -4,5 +4,5 @@ const SimpleMultiSigContract = artifacts.require("SimpleMultiSig");
 
 module.exports = function (deployer) {
     // コントラクトをデプロイする。
-    deployer.deploy (SimpleMultiSigContract);
+    deployer.deploy (SimpleMultiSigContract, 2, ["0x5386a3BA6D3512B3c718b23A0E99461e0f1251B3", "0x910cfbC84B035A23bF413185F73fa8C741Ae367a"], 5777);
 }
