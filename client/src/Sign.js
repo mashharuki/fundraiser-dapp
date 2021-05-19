c3eaeaf9742368d2fa075a7f386c52534312eaf1
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-05-19 18:23:47 +0900

    署名データが生成できるように修正しました。

--- client/src/Sign.js
+++ client/src/Sign.js
@@ -35,9 +35,10 @@ const Sign = () => {
     const [ gasLimit, setGasLimit ] = useState(null);
     const [ contract, setContract] = useState(null);
     const [ accounts, setAccounts ] = useState(null);
-    const [sigR, setSigR ] = useState([]);
-    const [sigS, setSigS ] = useState([]);
-    const [sigV, setSigV ] = useState([]);
+    const [ sigR, setSigR ] = useState([]);
+    const [ sigS, setSigS ] = useState([]);
+    const [ sigV, setSigV ] = useState([]);
+    const [ salt, setSalt ] = useState("0x251543af6a222378665a76fe38dbceae4871a070b7fdaf5c6c30cf758dc33cc0");
 
     // コンポーネントを用意する。
     useEffect (() => {
@@ -68,8 +69,72 @@ const Sign = () => {
 
     // handleSign関数(署名実行)
     const handleSign = async () => {
+        // 定数をドメインを設定する。
+        const domain = [
+            { name: "name", type: "string" },
+            { name: "version", type: "string" },
+            { name: "chainId", type: "uint256" },
+            { name: "verifyingContract", type: "address" },
+            { name: " salt", type: "bytes32" }
+        ];
+        // 定数マルチシグトランザクションを設定する。
+        const multiSigTx = [
+            { name: "destination", type: "address" },
+            { name: "value", type: "uint256" },
+            { name: "data", type: "bytes" },
+            { name: "nonce", type: "uint256" },
+            { name: "executor", type: "address" },
+            { name: "gasLimit", type: "uint256" }
+        ];
+  
+        const domainData = {
+            name: "Simple MultiSig",
+            version: "1",
+            chainId: parseInt(web3.version.network, 10),
+            verifyingContract: walletAddresss,
+            salt: salt
+        };
+        // メッセージ
+        var message = {
+            destination: destination,
+            value: value,
+            data: data,
+            nonce: parseInt(nonce, 10),
+            executor: executor,
+            gasLimit: parseInt(gasLimit, 10),
+        };
+        // 上記で設定した値をJSON形式のデータに変換する。
+        const data = JSON.stringify({
+            types: {
+                EIP712Domain: domain,
+                MultiSigTransaction: multiSigTx
+            },
+            domain: domainData,
+            primaryType: "MultiSigTransaction",
+            message: message
+        });
+        //　署名者を取得する。
+        const signer = accounts[0];
+        // sendAsync関数を実行する。
+        web3.currentProvider.sendAsync(
+            {
+                method: "eth_signTypedData_v3",
+                params: [signer, data],
+                from: signer
+            }, 
+            function(err, result) {
+                // エラーであればコンソールにその旨表示して終了
+                if (err || result.error) {
+                    return console.error(result);
+                }
+                // 署名データを作成する。
+                const signature = parseSignature(result.result.substring(2));
+                // 解析した署名内容を出力する。
+                document.getElementById("signedData").value = "r: " + signature.r + "\ns: " + signature.s + "\nv: " + signature.v;
+            }
+        );
         // コントラクトのexecuteを呼び出す。
-        await contract.methods.execute(sigV, sigR, sigS, destination, value, data, executor, gasLimit).send({ from: accounts[0] });
+        //await contract.methods.execute(sigV, sigR, sigS, destination, value, data, executor, gasLimit).send({ from: accounts[0] });
         // アラートを出す。
         alert('Successfully signed data')
     }
