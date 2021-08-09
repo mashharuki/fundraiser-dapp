ccb3b8070b168d36682fa2d13e5e8434a9ed85f5
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-08-09 16:09:39 +0900

    NFT用のコントラクトを追加しました。

---
+++ contracts/NFT.sol
@@ -0,0 +1,17 @@
+/**
+ * NFTを発行するためのコントラクトを実装したファイル
+ */
+
+pragma solidity >0.4.23;
+
+// 必要なモジュールをインポートする。
+import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+
+/**
+ * NFTコントラクトを定義する。
+ * (ERC721PresetMinterPauserAutoIdを継承して実装)
+ */
+contract NFT is ERC721PresetMinterPauserAutoId {
+    // コンストラクター 
+    constructor() ERC721PresetMinterPauserAutoId("name", "symbol", "http://localhost:3000") {}
+}
