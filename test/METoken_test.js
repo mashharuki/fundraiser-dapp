34f89e3aa9229e6a2d312e0b38e76560f7209592
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-08-15 12:39:49 +0900

    METoken用のテストコードを追加する。

---
+++ test/METoken_test.js
@@ -0,0 +1,21 @@
+/**
+ * METoken用のテストコード
+ */
+
+// METokenコントラクトを読み込んでインスタンス化する。
+const METokenContract = artifacts.require("METoken");
+// METFaucetコントラクトを読み込んでインスタンス化する。
+const METFaucetContract = artifacts.require("METFaucet");
+
+/**
+ * デプロイ関連のテストコード
+ */ 
+contract ("METoken & METFaucet Factory: deployment", () => {
+    // デプロイ済みかチェックする。
+    it ("has been deployde", async () => {
+        const METokenFactory = METokenContract.deployed();
+        const METFaucetFactory = METFaucetContract.deployed();
+        assert(METokenFactory, "METoken factory was not deployed");
+        assert(METFaucetFactory, "METFaucet factory was not deployed");
+    });
+});
