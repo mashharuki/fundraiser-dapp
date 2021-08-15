ea5424c4efc7ebd508c860fe4ba62c307240e8bf
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-08-15 18:31:54 +0900

    修正

--- client/src/Nft.js
+++ client/src/Nft.js
@@ -54,7 +54,7 @@ const Nft = () => {
             setWeb3(web3);
             // コントラクトをセットする。
             setContract(instance);
-            alert(instance.methods.name().call());
+            alert(instance.options.jsonInterface);
             alert(contract);
             // アカウントをセットする。
             setAccounts(accounts);
