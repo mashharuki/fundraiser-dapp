1076ebed17c4009d623ded9583461695d16e1f35
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-08-15 12:29:40 +0900

    METoken用のコントラクトを追加しました。

---
+++ contracts/METoken.sol
@@ -0,0 +1,34 @@
+/**
+ * 独自コイン「MET」を発行するためのコントラクト
+ */
+pragma solidity >= 0.4.21;
+
+import 'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+
+/**
+ * METokenコントラクト
+ * ERC020のStandardTokenを継承する。
+ */
+contract METoken is StandardToken {
+    // トークン名
+    string public constant name = 'Mastering Ethereum Token';
+    // シンボル名
+    string public constant symbol = 'MET';
+    // 2
+    uint8 public constant decimals = 2;
+    // 初期供給量
+    uint constant _initial_supply = 2100000000;
+    
+    /**
+     * コンストラクター
+     */
+    function METoken() public {
+        // 初期供給量
+        totalSupply_ = _initial_supply;
+        // 残高を設定
+        balances[msg.sender] = _initial_supply;
+        // イベントの呼び出し
+        emit Transfer(address(0), msg.sender, _initial_supply);
+    }
+}
+
