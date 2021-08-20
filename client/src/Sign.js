9703efe1d9ed8f1c6569eb7d6d94b6d8a3e73d4c
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-08-20 22:03:56 +0900

    修正

--- client/src/Sign.js
+++ client/src/Sign.js
@@ -119,7 +119,7 @@ const Sign = () => {
          * sendAsync関数を実行する。
          * methodには、eth_signTypedData_v3を指定する。
          */
-        web3.currentProvider.sendAsync({
+         web3.eth.sendTransaction({
                 jsonrpc: '2.0',
                 method: "eth_signTypedData_v3",
                 params: [signer, signedData],
