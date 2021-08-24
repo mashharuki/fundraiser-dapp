481302874c7836a3679d02e7c5806b3d3cbe5a17
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-08-24 23:02:48 +0900

    NFTコントラクトの機能を拡充する。

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -13,9 +13,41 @@ import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAut
  */
 contract NFT is ERC721PresetMinterPauserAutoId {
 
+    // NFT名
+    string name;
+    // シンボル名
+    string symbol;
+    // URL
+    string url;
+
     /**
      * コンストラクター 
      */
-    constructor(string memory name, string memory symbol, string memory url) ERC721PresetMinterPauserAutoId(name, symbol, url) {}
+    constructor(string memory name_, string memory symbol_, string memory url_) ERC721PresetMinterPauserAutoId(name_, symbol_, url_) {
+        // それぞれ値をセットする。
+        name = name_;
+        symbol = symbol_;
+        url = url_;
+    }
 
+    /**
+     * NFT名を取得する関数
+     */
+    function getName() view public returns (string memory) {
+        return name;
+    }
+
+    /**
+     * シンボル名を取得する関数
+     */
+    function getSymbol() view public returns (string memory) {
+        return symbol;
+    }
+
+    /**
+     * NFTに紐づけられているURLを取得する。
+     */
+    function getURL() view public returns (string memory) { 
+        return url;
+    }
 }
