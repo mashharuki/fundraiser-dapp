61fb2c27400af297276b4f199a0d3466868a95c4
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-08-27 23:07:38 +0900

    NFT用のダイアログコンポーネントを更新しました。

--- client/src/NFTCard.js
+++ client/src/NFTCard.js
@@ -66,7 +66,184 @@ const useStyles = makeStyles (theme => ({
  * NFTCardコンポーネント本体
  */
  const NFTCard = (props) => {
+     // 変数を定義する。
+     const { nft } = props; 
+     // スタイル用のクラス
+     const classes = useStyles();
+     // ステート変数を用意する。
+    const [ web3, setWeb3 ] = useState(null);
+    const [ nftName, setNftName ] = useState(null);
+    const [ nftSymbol, setNftSymbol ] = useState(null);
+    const [ nftURL, setNftURL ] = useState(null);
+    const [ contract, setContract] = useState(null);
+    const [ accounts, setAccounts ] = useState(null);
+    const [ tokenId , setTokenId] = useState(null);
+    const [ open, setOpen ] = useState(false);
 
+    /**
+     * useEffect関数
+     */
+    useEffect (() => {
+        // fundraiserが存在する時のみinit関数を実行する。
+        if (nft) {
+            init (nft);
+        }
+    }, [nft]);
+
+    /**
+     * init関数
+     */
+    const init = async (nft) => {
+        try {
+            // NFTコントラクトの情報を取得する。
+            const NFT = nft;
+            // Web3が使えるように設定する。
+            const provider = await detectEthereumProvider();
+            const web3 = new Web3(provider);
+            const networkId = await web3.eth.net.getId();
+            const deployedNetwork = NFTContract.networks[networkId];
+            const accounts = await web3.eth.getAccounts();
+            const instance = new web3.eth.Contract(NFTContract.abi, NFT);
+            // Web3をセットする。
+            setWeb3 (web3);
+            // コントラクトをセットする。
+            setContract (instance);
+            // アカウントをセットする。
+            setAccounts (accounts);
+            // NFTの名前、シンボル、URLを取得する。
+            const name = await instance.methods.getNftName().call();
+            const symbol = await instance.methods.getNftSymbol().call();
+            const url = await instance.methods.getNftURL().call();
+            // ステート変数にセットする。
+            setNftName(name);
+            setNftSymbol(symbol);
+            setNftURL(url);
+        } catch (error) {
+            alert(`Failed to load web3, accounts, or contract. Check console for details.`,);
+            console.error(error);
+        }
+        
+    }
+
+    /**
+     * アカウントが切り替わったら画面を更新する。
+     */
+    window.ethereum.on('accountsChanged', function (accounts) {
+        window.location.reload()
+    })
+
+    /**
+     * handleOpen関数
+     */
+    const handleOpen = () => {
+        // trueにして開く。
+        setOpen(true);
+    };
+
+    /**
+     * handleClose関数
+     */
+    const handleClose = () => {
+        // falseにして閉じる。
+        setOpen(false);
+    };
+
+    /**
+     * 「NFT発行」ボタンを押した時の処理
+     */
+     const buttonMint = async() => {
+        // プロバイダーから署名者の情報を取得する。
+        const signer = accounts[0];
+        // コントラクト
+        const networkId = await web3.eth.net.getId();
+        const deployedNetwork = NFTFactoryContract.networks[networkId];
+        const instance = new web3.eth.Contract(NFTFactoryContract.abi, deployedNetwork && deployedNetwork.address,);
+        // NFTコントラクトのmint関数を実行する。
+        const { hash } = await instance.methods.mint(signer).send({ from: signer });
+        // ネットワーク情報を取得する。
+        const net = NFTFactoryContract.networks[netID];
+        // rinkebyだった場合出力する。
+        if( net.chainId == 4) {
+            alert("https://rinkeby.etherscan.io/tx/" + hash);
+        }
+    }
+  
+    /**
+     * 「NFT総供給量取得」ボタンを押した時の処理
+     */
+    const buttonSupply = async() => {
+        // コントラクト
+        const networkId = await web3.eth.net.getId();
+        const deployedNetwork = NFTFactoryContract.networks[networkId];
+        const instance = new web3.eth.Contract(NFTFactoryContract.abi, deployedNetwork && deployedNetwork.address,);
+        // 総供給量を取得する。
+        const totalSupply = await instance.methods.totalSupply().call();
+        // totalSupply関数を呼び出す。
+        alert("総供給量：", totalSupply);
+    }
+
+    /**
+     * 「NFT数取得」ボタンを押した時の処理
+     */
+    const buttonBalanceOf = async() => {
+        // コントラクト
+        const networkId = await web3.eth.net.getId();
+        const deployedNetwork = NFTFactoryContract.networks[networkId];
+        const instance = new web3.eth.Contract(NFTFactoryContract.abi, deployedNetwork && deployedNetwork.address,);
+        // 接続中のアカウントに紐づくNFT数を取得する
+        const balanceOf = await instance.methods.balanceOf(accounts[0]).call();
+        // totalSupply関数を呼び出す。
+        alert("NFT数：", balanceOf);
+    }
+
+    /**
+     *  「所有者確認」ボタンを押した時の処理
+     */
+    const buttonOwnerOf = async() => {
+        // コントラクト
+        const networkId = await web3.eth.net.getId();
+        const deployedNetwork = NFTFactoryContract.networks[networkId];
+        const instance = new web3.eth.Contract(NFTFactoryContract.abi, deployedNetwork && deployedNetwork.address,);
+        // 所有者アドレスを取得する。
+        const ownerAddress = await instance.methods.ownerOf(tokenId).call();
+        alert("所有者アドレス：", ownerAddress);
+    }
+
+    // 戻り値
+    return (
+        <div className="fundraiser-card-content">
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">
+                    {nftName}
+                </DialogTitle>
+                <DialogContent>
+                    <p>
+                        SYMBOL : {nftSymbol}
+                    </p>
+                    <p>
+                        NFTURL : {nftURL}
+                    </p>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={buttonMint} variant="contained" color="primary" className={classes.button}>
+                        NFT発行
+                    </Button>
+                    <br/>
+                    <Button onClick={buttonBalanceOf} variant="contained" color="primary" className={classes.button}>
+                        NFT数取得
+                    </Button>
+                    <br/>
+                    <Button onClick={buttonSupply} variant="contained" color="primary" className={classes.button}>
+                        NFT総供給量取得
+                    </Button>
+                    <TextField id="outlined-bare4" className={classes.textField} placeholder="TokenId" margin="normal" onChange={ (e) => setTokenId(e.target.value) } variant="outlined" inputProps={{ 'aria-label': 'bare' }} />
+                    <Button onClick={buttonOwnerOf} variant="contained" color="primary" className={classes.button}>
+                        所有者確認
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
  }
 
  // コンポーネントを外部に公開する
