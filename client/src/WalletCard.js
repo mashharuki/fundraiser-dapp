782a1e09831c5ad885aa00fee584ebb8944b5fd6
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-08-31 23:01:42 +0900

    アドレスが取得できるように修正

--- client/src/WalletCard.js
+++ client/src/WalletCard.js
@@ -102,9 +102,9 @@ const WalletCard = (props) => {
             setContract (instance);
             // アカウントをセットする。
             setAccounts (accounts);
-            alert(gonosisSafe);
+            alert(instance.address);
             // コントラクトの情報を格納する。
-            setAddress(gonosisSafe);
+            setAddress(instance.address);
             // ウォレット名を取得する。
             const walletName = await instance.methods.getWalletName().call();
             // ステート変数にセットする。
