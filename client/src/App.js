d5c98eb5f3a921589becae04c1e24e365e8f4937
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-02-26 21:30:58 +0900

    メニューアイコンを作成

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, NavLink, RouteProps } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, NavLink, Link } from "react-router-dom";
 import FundraiserFactoryContract from "./contracts/FundraiserFactory.json";
 import getWeb3 from "./getWeb3";
 import "./App.css";
@@ -21,15 +21,45 @@ import Collection from "./nft/Collection";
 import AppBar  from '@mui/material/AppBar';
 import Toolbar  from '@mui/material/Toolbar';
 import Typography  from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
-// コンポーネントを用意する。
+// Menu用の配列
+const options = [
+  'Home',
+  'New',
+  'CreateSafeWallet',
+  'Sign',
+  'Ecrecover',
+  'Nft',
+  'Collection',
+  'MyToken',
+];
+// Menuコンポーネント用の定数
+const ITEM_HEIGHT = 48;
+
+/**
+ * Appコンポーネント
+ */
 const App = () => {
   // ステート変数を用意する。　
   const [ state, setState ] = useState({ web3: null, accounts: null, contract: null  });
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  // メニュー用の変数
+  const open = Boolean(anchorEl);
+  // スタイルシートを適用するためのコンポーネント読み込み
   const classes = UseStyles();
 
-  // useeffect関数
+  /**
+   * 副作用フック
+   */
   useEffect (() => {
+
+    /**
+     * init関数
+     */
     const init = async () => {
       try {
         // 変数を設定
@@ -52,6 +82,21 @@ const App = () => {
     init();
   }, []);
 
+  /**
+   * メニューアイコンをクリックした時の処理
+   * @param {*} event イベントハンドラ
+   */
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  /**
+   * メニューバーを閉じる時の処理
+   */
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   // レンダリングする内容
   return (
     <Router>
@@ -62,19 +107,43 @@ const App = () => {
               <strong>Web3.0 App</strong>
             </Typography>
             <Typography variant="h6" color="inherit">
-              <NavLink className={classes.navLink} to="/">Home</NavLink>
-              <NavLink className={classes.navLink} to="/new">New</NavLink>
-              <NavLink className={classes.navLink} to="/createSafeWallet">CreateSafeWallet</NavLink>
-              <NavLink className={classes.navLink} to="/sign">Sign</NavLink>
-              <NavLink className={classes.navLink} to="/ecrecover">Ecrecover</NavLink>
-              <NavLink className={classes.navLink} to="/nft">Nft</NavLink>
-              <NavLink className={classes.navLink} to="/collection">Collection</NavLink>
-              <NavLink className={classes.navLink} to="/myToken">MyToken</NavLink>
+              <IconButton 
+                aria-label="more"
+                id="home-menu-button"
+                aria-controls={open ? 'home-menu-button' : undefined}
+                aria-expanded={open ? 'true' : undefined}
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="web3.0-menu"
+                MenuListProps={{
+                  'aria-labelledby': 'home-menu-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                PaperProps={{
+                  style: {
+                    maxHeight: ITEM_HEIGHT * 4.5,
+                    width: '20ch',
+                  },
+                }}
+              >
+                {options.map((option) => (
+                  <MenuItem key={option} selected={option === 'Home'} onClick={handleClose}>
+                    <NavLink className={classes.navLink} to={{ pathname: `/${option}` }}>{option}</NavLink>
+                  </MenuItem>
+                ))}
+            </Menu>
             </Typography>
           </Toolbar>
         </AppBar>
         <Routes>
           <Route path="/" exact element={ <Home/> } />
+          <Route path="/home" exact element={ <Home/> } />
           <Route path="/new" element={ <NewFundraiser/> } />
           <Route path="/createSafeWallet" element={ <CreateSafeContractWallet/>} />
           <Route path="/receipts" element={ <Receipts/> } />
