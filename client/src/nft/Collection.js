d048c0f89bad501aa10fc4e44a8884ccc44f80ab
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-03-06 16:00:51 +0900

    余分なものを消去

--- client/src/nft/Collection.js
+++ client/src/nft/Collection.js
@@ -5,16 +5,9 @@
 // 必要なモジュールを読み込む
 import React, { useState, useEffect } from "react";
 import Web3 from 'web3';
-import NFTContract from './../contracts/NFT.json';
 import NFTFactoryContract from './../contracts/NFTFactory.json';
 import detectEthereumProvider from '@metamask/detect-provider';
 import CollectionCard from './CollectionCard';
-// Cardコンポーネントを読み込む
-import Card  from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 import UseStyles from "../common/useStyles";
 
 /**
